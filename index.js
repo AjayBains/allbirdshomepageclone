6702380a3b3ad8666a15f8134694fdda08fd5b9b
Ajay <ajaybains2004@gmail.com>
2021-10-20 20:55:46 +0530

    slider functionality added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const tabs = document.querySelector(".tabs");
 const tabsContent = document.querySelectorAll(".tabs__content--info");
 
-console.log(tabsContent);
+// console.log(tabsContent);
 const handleClick = (e) => {
   const clicked = e.target.dataset.tab;
   console.log(clicked);
@@ -12,6 +12,33 @@ const handleClick = (e) => {
 
   tab.classList.remove("hidden");
 
-  console.log(tab);
+  // console.log(tab);
 };
 tabs.addEventListener("click", handleClick);
+
+// SLIDER JS
+
+const card = document.querySelectorAll(".collections__items--card");
+const collectionItems = document.querySelector(".collections__items");
+const btnLeft = document.querySelector(".btn__left");
+const btnRight = document.querySelector(".btn__right");
+let count = 0;
+let distance;
+
+const handleLeftClick = () => {
+  if (count > 0 && count <= card.length / 2) {
+    distance = (count - 1) * 400;
+    collectionItems.style.transform = `translateX(-${distance}px)`;
+    count--;
+  }
+};
+const handleRightClick = () => {
+  if (count < card.length / 2) {
+    distance = (count + 1) * 400;
+    collectionItems.style.transform = `translateX(-${distance}px)`;
+    count++;
+  }
+};
+
+btnLeft.addEventListener("click", handleLeftClick);
+btnRight.addEventListener("click", handleRightClick);
